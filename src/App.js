9b3ebe9f3ed06dd1f1b45da9d8aeaca0e7451b9b
Cristian Parra <cristian.a.parra@gmail.com>
2020-08-25 21:28:49 -0400

    creaction de columnas y formulario en components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Formulario from './components/formulario'
 
 
 function App() {
   return (
-   <h1>Administrador de Pacientes</h1>
+    <Fragment>
+
+      <h1>Administrador de Pacientes</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='one-half column'>
+            <Formulario />
+          </div>
+          <div className='one-half column'>
+            <Formulario />
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </Fragment>
   );
 }
 
